1c32b3f76746ec91f08d039706c150b7c79d960d
Stanislav Svědiroh <standa.svediroh@gmail.com>
2023-01-22 22:36:55 +0100

    initial commit

---
+++ binary_classification.py
@@ -0,0 +1,107 @@
+import torch
+from torch import nn
+import sklearn
+from sklearn.datasets import make_circles
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+from pathlib import Path
+from helper_functions import plot_decision_boundary
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+
+if __name__ == '__main__':
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cpu"
+    print(f"Using device: {device}")
+
+    if Path("./helper_functions.py").is_file():
+        print("helper_function.py already exists")
+    else:
+        print("downloading helper_functions.py")
+        request = requests.get(
+            "https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+        with open("./helper_functions.py", "wb") as f:
+            f.write(request.content)
+
+    n_samples = 1000
+
+    X, y = make_circles(n_samples, noise=0.03, random_state=42)
+
+    circles = pd.DataFrame({"X1": X[:, 0], "X2": X[:, 1], "label": y})
+    plt.scatter(x=X[:, 0], y=X[:, 1], c=y, cmap=plt.cm.RdYlBu)
+    plt.show()
+
+    X = torch.from_numpy(X).type(torch.float)
+    y = torch.from_numpy(y).type(torch.float)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train = X_train.to(device)
+    y_train = y_train.to(device)
+    X_test = X_test.to(device)
+    y_test = y_test.to(device)
+
+    model = nn.Sequential(
+        nn.Linear(in_features=2, out_features=10),
+        nn.LeakyReLU(),
+        nn.Linear(in_features=10, out_features=10),
+        nn.LeakyReLU(),
+        nn.Linear(in_features=10, out_features=1),
+    )
+    model = model.to(device)
+
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
+
+    epoch_no = []
+    training_loss = []
+    testing_loss = []
+    accuracy = []
+    test_accuracy = []
+
+    epochs = 1000
+
+    for epoch in range(epochs):
+        model.train()
+
+        y_logits = model(X_train).squeeze()
+        y_pred = torch.round(torch.sigmoid(y_logits))
+
+        loss = loss_fn(y_logits, y_train)
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        model.eval()
+        with torch.inference_mode():
+            test_logits = model(X_test).squeeze()
+            test_pred = torch.round(torch.sigmoid(test_logits))
+            test_loss = loss_fn(test_logits, y_test)
+
+        acc = accuracy_fn(y_true=y_train, y_pred=y_pred)
+        test_acc = accuracy_fn(y_true=y_train, y_pred=y_pred)
+
+        print(f"Epoch: {epoch} | Loss: {loss} | Acc: {acc} | Test loss: {test_loss} | Test Acc: {test_acc}")
+        epoch_no.append(epoch)
+        training_loss.append(loss.cpu())
+        testing_loss.append(test_loss.cpu())
+        accuracy.append(acc)
+        test_accuracy.append(test_acc)
+
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title("Train")
+    plot_decision_boundary(model, X_train, y_train)
+    plt.subplot(1, 2, 2)
+    plt.title("Test")
+    plot_decision_boundary(model, X_test, y_test)
+    plt.show()
---
+++ main.py
@@ -0,0 +1,104 @@
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+def plot_predictions(train_data, train_labels, test_data, test_labels, predictions=None):
+    plt.figure(figsize=(10, 7))
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    if predictions is not None:
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+    plt.legend(prop={"size": 4})
+    plt.show()
+
+
+class LinearRegressionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_layer = nn.Linear(in_features=1, out_features=1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.linear_layer(x)
+
+
+if __name__ == '__main__':
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using device: {device}")
+
+    weight = 0.7
+    bias = 0.3
+
+    start = 0
+    end = 1
+    step = 0.02
+
+    X = torch.arange(start, end, step).unsqueeze(1)
+    y = X * weight + bias
+
+    train_split = int(0.8 * len(X))
+    X_train, y_train = X[:train_split], y[:train_split]
+    X_test, y_test = X[train_split:], y[train_split:]
+
+    X_train = X_train.to(device)
+    y_train = y_train.to(device)
+    X_test = X_test.to(device)
+    y_test = y_test.to(device)
+
+    model = LinearRegressionModel()
+    model.to(device)
+    print(model.state_dict())
+
+    loss_fn = nn.L1Loss()
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=0.005)
+
+    epochs = 2000
+
+    epoch_no = []
+    training_loss = []
+    testing_loss = []
+
+    for epoch in range(epochs):
+        model.train()
+
+        y_pred = model(X_train)
+
+        loss = loss_fn(y_pred, y_train)
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        model.eval()
+        with torch.inference_mode():
+            test_pred = model(X_test)
+
+            test_loss = loss_fn(test_pred, y_test)
+
+        print(f"Epoch: {epoch} | Loss: {loss} | Test loss: {test_loss}")
+        epoch_no.append(epoch)
+        training_loss.append(loss.cpu())
+        testing_loss.append(test_loss.cpu())
+
+    with torch.inference_mode():
+        plot_predictions(X_train.cpu(), y_train.cpu(), X_test.cpu(), y_test.cpu(), test_pred.cpu())
+        plt.plot(epoch_no, training_loss, label="Training loss")
+        plt.plot(epoch_no, testing_loss, label="Testing loss")
+        plt.show()
+
+    MODEL_PATH = Path("models")
+    MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+    MODEL_NAME = "pytorch_workflow_model.pth"
+    MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+    torch.save(obj=model.state_dict(), f=MODEL_SAVE_PATH)
+
+    # To load a model
+    # model = LinearRegressionModel()
+    # model.load_state_dict(torch.load(MODEL_SAVE_PATH))
